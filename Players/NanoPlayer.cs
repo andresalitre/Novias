f22d7224be4d04b07666c7396d0a6ab3701f82ee
Andrés Rojas <93074573+andresalitre@users.noreply.github.com>
2026-04-13 21:41:25 -0400

    Nano bug arreglado

--- Players/NanoPlayer.cs
+++ Players/NanoPlayer.cs
@@ -6,23 +6,17 @@ namespace Novias.Players
 {
     public class NanoPlayer : ModPlayer
     {
-        public bool Ayudada = false;
         public bool EstaSiguiendo = false;
         public bool HacerAnimacion = false;
-        public bool EsperandoDialogo = false;
 
         public override void SaveData(TagCompound tag)
         {
-            tag["Ayudada"] = Ayudada;
             tag["EstaSiguiendo"] = EstaSiguiendo;
-            tag["EsperandoDialogo"] = EsperandoDialogo;
         }
 
         public override void LoadData(TagCompound tag)
         {
-            Ayudada = tag.GetBool("Ayudada");
             EstaSiguiendo = tag.GetBool("EstaSiguiendo");
-            EsperandoDialogo = tag.GetBool("EsperandoDialogo");
         }
     }
 }
